79eb00713f967804864c91284ca83ef99a3afe1c
David Araújo <david2araujo5@gmail.com>
2023-11-17 15:45:19 +0000

    Rust generator finished

--- rust/random_generator/src/main.rs
+++ rust/random_generator/src/main.rs
@@ -1,7 +1,9 @@
+use hex;
 use pbkdf2::pbkdf2_hmac;
 use rc4::{consts::*, KeyInit, StreamCipher};
 use rc4::{Key, Rc4};
 use sha2::{Digest, Sha256};
+use std::env;
 
 fn is_sub<T: PartialEq>(haystack: &[T], needle: &[T]) -> bool {
     haystack.windows(needle.len()).any(|c| c == needle)
@@ -50,7 +52,7 @@ fn rand_byte_gen(
     confusion_string: &str,
     rounds: u32,
     seed_length: usize,
-    _byte_amount: usize,
+    byte_amount: usize,
 ) {
     let mut sha256 = Sha256::new();
     sha256.update(confusion_string);
@@ -64,13 +66,29 @@ fn rand_byte_gen(
         &mut seed,
     );
 
+    // Create confusion pattern base on confusion string
     let confusion_pattern = create_confusion_pattern(confusion_string);
 
+    // Compute a complex seed
     let complex_seed = complex_seed_generator(&seed, &confusion_pattern, rounds);
 
-    println!("Complex Seed: {:02X?}", complex_seed);
+    // Compute the requested number on random bytes using the complex seed
+    let key = Key::<U32>::from_slice(&complex_seed);
+    let mut rc4 = Rc4::<_>::new(key);
+    let mut buffer = vec![0_u8; byte_amount];
+    rc4.apply_keystream(&mut buffer);
+
+    println!("{}", hex::encode(buffer));
 }
 
 fn main() {
-    rand_byte_gen("password", "fg", 10000, 32, 10);
+    let args: Vec<String> = env::args().collect();
+
+    let password = &args[1];
+    let confusion_string = &args[2];
+    let rounds = args[3].parse::<u32>().unwrap();
+    let byte_length = args[4].parse::<usize>().unwrap();
+    let byte_amount = args[5].parse::<usize>().unwrap();
+
+    rand_byte_gen(password, confusion_string, rounds, byte_length, byte_amount);
 }
