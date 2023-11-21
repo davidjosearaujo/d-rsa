17374fee358a75130507a0913d798816559da488
David Araújo <david2araujo5@gmail.com>
2023-11-21 22:47:35 +0000

    Compute large primes

--- rust/d_rsa/src/main.rs
+++ rust/d_rsa/src/main.rs
@@ -9,5 +9,12 @@ fn main() {
     println!("{:?}", input);
 
     // TODO:
-    //  - Create RSA key pair
+    //  - Choose two large prime numbers p and q
+    //      -   https://docs.rs/is_prime/latest/is_prime/
+    //      -   https://docs.rs/num-bigint/latest/num_bigint/
+    //      -   https://medium.com/snips-ai/prime-number-generation-2a02f28508ff
+    //  - Compute n = pq
+    //  - Compute lambda(n), where lambda is Carmichael's totient function.
+    //  - Choose an integer e such that 2 < e < lambda(n) and gcd(e, lambda(n)) = 1, e and lambda(n) are coprime
+    //  - Determine d as d = e⁻¹(mod lambda(n))
 }
