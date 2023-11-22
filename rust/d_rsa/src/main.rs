ccfd5116d5e8188476f0a9e58977345da597e11d
David Araújo <david2araujo5@gmail.com>
2023-11-22 17:28:52 +0000

    Reading bytes from stdin. Must be in hex format

--- rust/d_rsa/src/main.rs
+++ rust/d_rsa/src/main.rs
@@ -1,15 +1,20 @@
-use std::io::{stdin, Read};
-//use std::io::Read;
+use hex;
+use std::io;
 
 fn main() {
-    let mut input = Vec::new();
-    for byte in stdin().bytes(){
-        input.extend(byte);
-    }
-    println!("{:?}", input);
+    let mut input = String::new();
+    let _ = io::stdin().read_line(&mut input).unwrap();
+
+    // Remove trailing newline and encode to Vec<u8>
+    input.pop();
+    let random_bytes = hex::decode(input).unwrap();
+
+    println!("{:02X?}", random_bytes);
 
     // TODO:
-    //  - Choose two large prime numbers p and q
+    //  - Read two a large prime number from the random_generator and find two prime number from it,
+    //      this can be accomplished by simply reading 2048 bytes and using half of that for each
+    //      variable p and q.
     //      -   https://docs.rs/is_prime/latest/is_prime/
     //      -   https://docs.rs/num-bigint/latest/num_bigint/
     //      -   https://medium.com/snips-ai/prime-number-generation-2a02f28508ff
--- rust/random_generator/src/main.rs
+++ rust/random_generator/src/main.rs
@@ -1,9 +1,17 @@
 use std::env;
+use std::process;
+use hex;
 
 mod random_generator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() != 6 || &args[1] == "-h" {
+        let help = "Random number generator\nUsage:\n\trandom_generator <password> <confusion-string> <rounds> <seed-byte-amount> <random-byte-amount>";
+        println!("{}", help);
+        process::exit(0x1);
+    }
 
     let password = &args[1];
     let confusion_string = &args[2];
@@ -11,8 +19,13 @@ fn main() {
     let byte_length = args[4].parse::<usize>().unwrap();
     let byte_amount = args[5].parse::<usize>().unwrap();
 
-    let buffer = random_generator::rand_byte_gen(password, confusion_string, rounds, byte_length, byte_amount);
+    let buffer = random_generator::rand_byte_gen(
+        password,
+        confusion_string,
+        rounds,
+        byte_length,
+        byte_amount,
+    );
 
-    // TESTING
-    println!("{:?}", buffer);
+    println!("{}", hex::encode(buffer));
 }
